c9272b573e2d21da2f5d9f4ee188855e3a5c6f67
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-03-20 22:51:03 +0530

    Moving Logo component from header to componts folder for reusablity

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import styles from "./Header.module.css";
-import Logo from "./Logo/Logo/";
+import Logo from "../Logo/Logo";
 import SearchBar from "./SearchBar/SearchBar";
 
 const Header = () => {
